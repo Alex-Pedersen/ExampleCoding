248e78d3a04b7de12485ec0d8411f62e097182f4
Alex-Pedersen <1038281@ucn.dk>
2016-02-11 10:07:44 +0100

    Example of delegation done

--- delegateExample/delegateExample/Program.cs
+++ delegateExample/delegateExample/Program.cs
@@ -1,15 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace delegateExample
 {
-    class Program
+    internal delegate int NumberChanger(int n);
+
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static int _num = 10;
+
+        private static int AddNum(int p)
         {
+            _num += p;
+            return _num;
+        }
+
+        private static int MultNum(int q)
+        {
+            _num *= q;
+            return _num;
+        }
+
+        private static int GetNum()
+        {
+            return _num;
+        }
+
+        private static void Main(string[] args)
+        {
+            //create delegate instances
+            NumberChanger nc1 = AddNum;
+            NumberChanger nc2 = MultNum;
+            var nc3 = AddNum(5);
+
+            //calling the methods using the delegate objects
+            nc1(25);
+            Console.WriteLine("Value of Num: {0}", GetNum());
+            nc2(5);
+            Console.WriteLine("Value of Num: {0}", GetNum());
+            Console.ReadKey();
         }
     }
 }
